c4ac27e1b7b4608fc7a0e8949ae70c31ec1c06f3
Stefis0702 <stefis0528@gmail.com>
2024-01-30 23:52:17 +0100

    Maquetacion-Home

--- src/pages/CotizacionPage.tsx
+++ src/pages/CotizacionPage.tsx
@@ -1,6 +1,5 @@
 import NavComponent2 from "../Components/Navbar/navbar2";
 import FormularioCotizacion from "../Components/Cotizacion/cotizacion";
-import imgCoti from "../assets/img/imgCoti.png";
 import FooterComponent from "../Components/Footer/Footer";
 
 function CotizacionPage(): JSX.Element {
@@ -9,7 +8,7 @@ function CotizacionPage(): JSX.Element {
       <NavComponent2 />
       <div className="flex flex-wrap">
         <div className="w-full md:w-1/2 mt-8">
-          <img src={imgCoti} alt="" className=" max-w-2xl ml-10 md:mr-0 " />
+          <img src="" alt="" className=" max-w-2xl ml-10 md:mr-0 " />
         </div>
         <div className="w-full md:w-1/2">
           <FormularioCotizacion />
