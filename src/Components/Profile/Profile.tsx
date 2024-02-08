b4cb70dcb05a3853694ecdbe9e018155214d63fa
Stefis0702 <stefis0528@gmail.com>
2024-02-08 12:16:57 +0100

    botonyprofile

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,7 +1,8 @@
 import { useAuth } from "../../Context/AuthContext";
+import capturaprofile from "../../assets/img/CapturaProfile.png";
 
 function ProfileComponent() {
-  const { user, logout, loading } = useAuth();
+  const { logout, loading } = useAuth();
 
   const handleLogout = async () => {
     await logout();
@@ -10,13 +11,18 @@ function ProfileComponent() {
   if (loading) return <h1>Loading...</h1>;
 
   return (
-    <div>
-      <h1>Profile {user.displayName}</h1>
-      <p>Welcome {user.email}</p>
-
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <div className=" min-h-screen bg-cover bg-center" style={{ 
+      backgroundImage: `url(${capturaprofile})`,
+      backgroundSize: '85%',
+    }}>
+     
+      {/* <p className="text-white">Welcome {user.email}</p> */}
+       <button onClick={handleLogout} className="absolute left-[20rem] -bottom-[12rem] bg-navbar2 hover:bg-navbar text-texto font-bold py-2 px-11 rounded">Logout</button>
+      </div>
+      
+    
   );
 }
 
 export default ProfileComponent;
+
